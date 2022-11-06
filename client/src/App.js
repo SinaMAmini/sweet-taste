b88c680bea27c5d4de9750704002ed8cc8bcb39f
SinaMAmini <smobasheramini@gmail.com>
2022-11-06 22:06:43 +0330

    [ADD] build property and much more

--- client/src/App.js
+++ client/src/App.js
@@ -2,34 +2,52 @@ import './App.css';
 import Nav from './components/Nav';
 import Login from './login/Login';
 import Signup from './login/Signup';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import Food from './menu/Food';
 import Home from './Home';
 import React from 'react';
 import Error from './components/Error';
 import Axios from 'axios';
-import { useEffect,useState } from 'react';
+import {useEffect, useState} from 'react';
+import {ThemeProvider} from 'styled-components';
+import {useDarkMode} from './useDarkMode';
+import {GlobalStyles} from './globalStyles';
+import {lightTheme, darkTheme} from './Thems';
+import Toggle from './Toggler';
 
 function App() {
-
     const [foodBlog, setFoodBlog] = useState([]);
     useEffect(async () => {
         const response = await Axios.get('http://localhost:3001/foods/all', {});
         setFoodBlog(response.data);
     }, []);
 
+    const time = new Date();
+    const hour = time.getHours();
+    const [theme, themeToggler, mountedComponent] = useDarkMode();
+    const themeMode = theme === 'light' ? lightTheme : darkTheme;
+    if (!mountedComponent) return <div />;
+
     return (
-        <div className="App">
-            <Nav />
-            <Switch>
-                <Route path="/login" component={Login} />
-                <Route path="/signup" component={Signup} />
-                {foodBlog.map((food) => <Route key={food.food_id} path={'/' + food.food_name}><Food food={food}></Food></Route>)}
-                <Route path="/Error"   component={Error}   />
-                <Route exact={true} path="/" component={Home} />
-                <Redirect to="/Error" />
-            </Switch>
-        </div>
+        <ThemeProvider theme={themeMode}>
+            <GlobalStyles />
+            <div className="App">
+                <Nav />
+                <Toggle theme={theme} toggleTheme={themeToggler} />
+                <Switch>
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={Signup} />
+                    {foodBlog.map((food) => (
+                        <Route key={food.food_id} path={'/' + food.food_name}>
+                            <Food food={food}></Food>
+                        </Route>
+                    ))}
+                    <Route path="/Error" component={Error} />
+                    <Route exact={true} path="/" component={Home} />
+                    <Redirect to="/Error" />
+                </Switch>
+            </div>
+        </ThemeProvider>
     );
 }
 
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -24,40 +24,40 @@ const Nav = () => {
 
     return (
         <div>
-        <nav className={styles.header}>
-            <div className={`${styles.logo}  navbar-brand mt-2  `}>
-                <Link to="/" title="cooking website">
-                    Sweet Taste{' '}
-                </Link>{' '}
-            </div>{' '}
-            <div className={`${styles.left}  px5 `}>
-                {' '}
-                {!name ? (
-                    <span className={styles.loginregister}>
-                        <Link to="/login" title="log-in">
-                            ورود{' '}
-                        </Link>{' '}
-                        <span id="back"> / </span>
-                        <Link to="/signup" title="sign-up">
-                            ثبت نام{' '}
-                        </Link>{' '}
-                    </span>
-                ) : (
-                    <span className={`${styles.greeting} col-10 col-md-6 navbar-brand  `}>
-                        {' '}
-                        !عزیز {name}
-                        سلام{' '}
-                    </span>
-                )}{' '}
-                {name && (
-                    <button className={`${styles.exitButton} navbar-brand `} onClick={logoutClickHandler}>
-                        <img className={styles.icon} src={heart} alt="" />
-                        خروج{' '}
-                    </button>
-                )}{' '}
-            </div>{' '}
-        </nav>{' '}
-    </div>
+            <nav className={styles.header}>
+                <div className={`${styles.logo}  navbar-brand mt-2  `}>
+                    <Link to="/" title="cooking website">
+                        Sweet Taste{' '}
+                    </Link>{' '}
+                </div>{' '}
+                <div className={`${styles.left}  px5 `}>
+                    {' '}
+                    {!name ? (
+                        <span className={styles.loginregister}>
+                            <Link to="/login" title="log-in">
+                                ورود{' '}
+                            </Link>{' '}
+                            <span id="back"> / </span>
+                            <Link to="/signup" title="sign-up">
+                                ثبت نام{' '}
+                            </Link>{' '}
+                        </span>
+                    ) : (
+                        <span className={`${styles.greeting} col-10 col-md-6 navbar-brand  `}>
+                            {' '}
+                            !عزیز {name}
+                            سلام{' '}
+                        </span>
+                    )}{' '}
+                    {name && (
+                        <button className={`${styles.exitButton} navbar-brand `} onClick={logoutClickHandler}>
+                            <img className={styles.icon} src={heart} alt="" />
+                            خروج{' '}
+                        </button>
+                    )}{' '}
+                </div>{' '}
+            </nav>{' '}
+        </div>
     );
 };
 
